8de473dbf3d6ab60a6fc5beacedfcdfd127e601a
Jason Park <platosw83@gmail.com>
2023-05-07 22:57:38 -0400

    init users table in model.py

--- model.py
+++ model.py
@@ -2,6 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User():
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String(30),
+                      unique=True,
+                      nullable=False)
+    password = db.Column(db.String,
+                         nullable=False)
+    username = db.Column(db.String(20),
+                         nullable=False)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id}, email={self.email}>'
+
 def connect_to_db(flask_app, db_uri="postgresql:///jayskine", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
