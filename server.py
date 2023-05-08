c4d47038789bbf1c81df08c66151bd8020c6a4a6
Jason Park <platosw83@gmail.com>
2023-05-08 13:07:21 -0400

    Created view categories, view notes functions in index route and also created detail pages of a category and a note

--- server.py
+++ server.py
@@ -5,11 +5,24 @@ app = Flask(__name__)
 from model import db, connect_to_db
 import crud
 
+
+# About show routes
 @app.route("/")
 def index():
     notes = crud.get_all_notes()
     categories = crud.get_all_categories()
     return render_template("index.html", notes=notes, categories=categories)
+
+
+@app.route("/categories/<category_id>")
+def show_detail_category(category_id):
+    category = crud.get_category(category_id)
+    return render_template("detail_category.html", category=category)
+
+@app.route("/notes/<note_id>")
+def show_detail_note(note_id):
+    note = crud.get_note(note_id)
+    return render_template("detail_note.html", note=note)
 
 
 if __name__ == "__main__":
