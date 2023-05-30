3cbf1e97b9eb17a4bc65e355a45483827793c329
Jason Park <platosw83@gmail.com>
2023-05-30 03:15:28 -0400

    Removed category_detail route and html file.

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import crud
+from model import db, connect_to_db
 import os
 from flask import Flask, render_template, request, flash, session, redirect, jsonify
 from flask_mde import Mde, MdeField
@@ -13,20 +15,20 @@ app.secret_key = "dev"
 mde = Mde(app)
 app.jinja_env.undefined = StrictUndefined
 
-from model import db, connect_to_db
-import crud
 
 class MdeForm(FlaskForm):
-     editor = MdeField()
-     submit = SubmitField()
+    editor = MdeField()
+    submit = SubmitField()
 
 
-# About show routes
+# This is a root route
 @app.route("/")
 def index():
     return render_template("index.html")
 
 # About authentication routes
+
+
 @app.route("/login", methods=["POST"])
 def login():
     email = request.form.get("user_email")
@@ -41,6 +43,7 @@ def login():
 
     return redirect("/")
 
+
 @app.route("/logout")
 def logout():
     if "user" in session:
@@ -51,27 +54,18 @@ def logout():
         return redirect("/")
 
 
-
+# This is only current login user's data api
 @app.route("/jayskine.api")
 def index_data():
     if "user" in session:
         # print(session['user'])
-        current_user_id = crud.get_user_by_email(session["user"]["email"]).user_id
+        current_user_id = crud.get_user_by_email(
+            session["user"]["email"]).user_id
         return jsonify([get_all_notes(current_user_id), get_all_categories(current_user_id)])
     else:
         return redirect('/')
 
-@app.route("/categories/<category_id>")
-def show_detail_category(category_id):
-    if "user" in session:
-        category = crud.get_category(category_id)
-        if session["user"]["email"] == category.user.email:
-            return render_template("detail_category.html", category=category)
-        else:
-             return redirect("/")
-    else:
-        return redirect("/")
-    
+
 @app.route("/categories.api/<category_id>")
 def detail_category_data(category_id):
     if "user" in session:
@@ -79,16 +73,20 @@ def detail_category_data(category_id):
         if session["user"]["email"] == category["user"]["email"]:
             return jsonify(category)
         else:
-             return redirect("/")
+            return redirect("/")
     else:
         return redirect("/")
-    
+
+# About show routes
+
+
 @app.route("/create_note")
 def show_create_note():
-     form = MdeForm()
-     current_user_id = crud.get_user_by_email(session["user"]["email"]).user_id
-     categories = get_all_categories(current_user_id)
-     return render_template("create_note.html", form=form, categories=categories)
+    form = MdeForm()
+    current_user_id = crud.get_user_by_email(session["user"]["email"]).user_id
+    categories = get_all_categories(current_user_id)
+    return render_template("create_note.html", form=form, categories=categories)
+
 
 @app.route("/notes/<note_id>")
 def show_detail_note(note_id):
@@ -101,42 +99,46 @@ def show_detail_note(note_id):
         return render_template("detail_note.html", note=note, categories=categories, content=content, form=form)
     else:
         return redirect("/")
-    
+
+
 @app.route("/users/<email>")
 def show_user(email):
     user = crud.get_user_by_email(email)
     return render_template("detail_user.html", user=user)
 
+
 @app.route("/tags")
 def show_tags():
-     
+
     return render_template("tags.html")
 
 
 # About create routes
 @app.route("/add_category", methods=["POST"])
 def add_category():
-        name = request.json.get("name")
-        user_email = session["user"]["email"]
-        user = crud.get_user_by_email(user_email)
-        new_category = crud.create_category(name, user)
-        db.session.add(new_category)
-        db.session.commit()
-        return ""
+    name = request.json.get("name")
+    user_email = session["user"]["email"]
+    user = crud.get_user_by_email(user_email)
+    new_category = crud.create_category(name, user)
+    db.session.add(new_category)
+    db.session.commit()
+    return ""
+
 
 @app.route("/add_note", methods=["POST"])
 def add_note():
-        title = request.form.get("note_title")
-        body_content = request.form.get("editor")
-        user_email = session["user"]["email"]
-        category_id = request.form.get("note_category_id")
-        tags = request.form.get("tags")
-        user = crud.get_user_by_email(user_email)
-        category = crud.get_category(category_id)
-        new_note = crud.create_note(title, body_content, user, category, tags)
-        db.session.add(new_note)
-        db.session.commit()
-        return redirect("/")
+    title = request.form.get("note_title")
+    body_content = request.form.get("editor")
+    user_email = session["user"]["email"]
+    category_id = request.form.get("note_category_id")
+    tags = request.form.get("tags")
+    user = crud.get_user_by_email(user_email)
+    category = crud.get_category(category_id)
+    new_note = crud.create_note(title, body_content, user, category, tags)
+    db.session.add(new_note)
+    db.session.commit()
+    return redirect("/")
+
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
@@ -157,65 +159,68 @@ def add_user():
         return redirect("/")
 
 
-
 # About update routes
 @app.route("/edit_category", methods=["POST"])
 def update_category():
-        id = request.json.get("id")
-        name = request.json.get("name")
-        updated_category = crud.update_category(id, name)
-        db.session.add(updated_category)
-        db.session.commit()
-        cat = get_category(id)
-        print(cat)
-        return jsonify(cat)
+    id = request.json.get("id")
+    name = request.json.get("name")
+    updated_category = crud.update_category(id, name)
+    db.session.add(updated_category)
+    db.session.commit()
+    cat = get_category(id)
+    print(cat)
+    return jsonify(cat)
+
 
 @app.route("/edit_note", methods=["POST"])
 def update_note():
-        id = request.form.get("note_id")
-        new_title = request.form.get("note_title")
-        new_body_content = request.form.get("body_content")
-        new_category_id = request.form.get("note_category")
-        new_tags = request.form.get("tags")
-        new_category = crud.get_category(new_category_id)
-        update_note = crud.update_note(id, new_title, new_body_content, new_category, new_tags)
-        db.session.add(update_note)
-        db.session.commit()
-        return redirect(f"/notes/{id}")
+    id = request.form.get("note_id")
+    new_title = request.form.get("note_title")
+    new_body_content = request.form.get("body_content")
+    new_category_id = request.form.get("note_category")
+    new_tags = request.form.get("tags")
+    new_category = crud.get_category(new_category_id)
+    update_note = crud.update_note(
+        id, new_title, new_body_content, new_category, new_tags)
+    db.session.add(update_note)
+    db.session.commit()
+    return redirect(f"/notes/{id}")
+
 
 @app.route("/edit_user", methods=["POST"])
 def update_user():
-     email = session["user"]["email"]
-     user = crud.get_user_by_email(email)
-     new_username = request.form.get("username")
-     new_password = request.form.get("password")
-     update_user = crud.update_user(user.user_id, new_password, new_username)
-     db.session.add(update_user)
-     db.session.commit()
-     session["user"] = {"email": user.email, "username": user.username}
-     return redirect(f"/users/{email}")
+    email = session["user"]["email"]
+    user = crud.get_user_by_email(email)
+    new_username = request.form.get("username")
+    new_password = request.form.get("password")
+    update_user = crud.update_user(user.user_id, new_password, new_username)
+    db.session.add(update_user)
+    db.session.commit()
+    session["user"] = {"email": user.email, "username": user.username}
+    return redirect(f"/users/{email}")
 
 
 # About Delete routes
 @app.route("/delete_user/<user_id>")
 def delete_user(user_id):
-     msg = crud.delete_user(user_id)
-     session.clear()
-     flash(msg)
-     return redirect("/")
+    msg = crud.delete_user(user_id)
+    session.clear()
+    flash(msg)
+    return redirect("/")
+
 
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
-     msg = crud.delete_category(category_id)
-     flash(msg)
-     return redirect("/")
+    msg = crud.delete_category(category_id)
+    flash(msg)
+    return redirect("/")
+
 
 @app.route("/delete_note/<note_id>")
 def delete_note(note_id):
-     msg = crud.delete_note(note_id)
-     flash(msg)
-     return redirect("/")
-
+    msg = crud.delete_note(note_id)
+    flash(msg)
+    return redirect("/")
 
 
 if __name__ == "__main__":
