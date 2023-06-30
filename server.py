78cd023dcf98a2ae9d5c95de2b7f10a38a4ff937
Jason Park <platosw83@gmail.com>
2023-06-30 11:23:28 -0400

    Restyling Markdown viewer.

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, flash, session, redirect, jso
 from flask_mde import Mde, MdeField
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
-import markdown
+from flaskext.markdown import Markdown
 import openai
 from transform_object_to_dictionary import get_all_notes, get_all_categories, get_category
 
@@ -14,6 +14,7 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEYS"]
 mde = Mde(app)
+Markdown(app, extensions=["nl2br", "fenced_code"])
 app.jinja_env.undefined = StrictUndefined
 app.static_folder = 'static'
 
@@ -100,7 +101,7 @@ def show_detail_note(note_id):
         form = MdeForm()
         note = crud.get_note(note_id)
         current_user = crud.get_user_by_email(session["user"]["email"])
-        content = markdown.markdown(note.body_content)
+        content = note.body_content
         categories = crud.get_all_categories(current_user.user_id)
         return render_template("detail_note.html", note=note, categories=categories, content=content, form=form)
     else:
